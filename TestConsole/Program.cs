aa1250a593a2d1e77a1f1ead0d2780498fce86c8
Administrator <Administrator@ACADEMY-6211VQH>
2017-03-22 16:38:14 +0100

    Hitta felen

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -35,59 +35,59 @@ namespace TestConsole
             Console.WriteLine("Ange ID nummer");
             string inputID = Console.ReadLine();
 
-            Console.WriteLine(SQL.GetContactById(inputID));
+            Console.WriteLine(SQL.GetContactById("7"));
         }
 
-        private static List<Contact> SearchContactIDByName()
-        {
-            List<Contact> findContacts;
+        //private static List<Contact> SearchContactIDByName()
+        //{
+        //    List<Contact> findContacts;
 
-            Console.WriteLine("Ange förnamn att söka");
-            string inputFirstName = Console.ReadLine();
+        //    Console.WriteLine("Ange förnamn att söka");
+        //    string inputFirstName = Console.ReadLine();
 
-            Console.WriteLine("Ange efternamn att söka");
-            string inputLastName = Console.ReadLine();
+        //    Console.WriteLine("Ange efternamn att söka");
+        //    string inputLastName = Console.ReadLine();
 
-            findContacts = SQL.SearchContactIDByName(inputFirstName, inputLastName);
+        //    findContacts = SQL.SearchContactIDByName(inputFirstName, inputLastName);
 
-            foreach (var contact in findContacts)
-            {
-                Console.WriteLine(contact.ToString());
-            }
+        //    foreach (var contact in findContacts)
+        //    {
+        //        Console.WriteLine(contact.ToString());
+        //    }
 
-            return findContacts;
-        }
+        //    return findContacts;
+        //}
 
-        private static void UpdateContact()
-        {
-            List<Contact> updatedContacts;
+        //private static void UpdateContact()
+        //{
+        //    List<Contact> updatedContacts;
 
-            Console.WriteLine("Ange ID att byta ut");
-            int findIDTOChange = Convert.ToInt32(Console.ReadLine());
+        //    Console.WriteLine("Ange ID att byta ut");
+        //    int findIDTOChange = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Ange förnamn att byta till: ");
-            string newfirstName = Console.ReadLine();
+        //    Console.WriteLine("Ange förnamn att byta till: ");
+        //    string newfirstName = Console.ReadLine();
 
-            Console.WriteLine("Ange efternamn att byta till: ");
-            string newlastName = Console.ReadLine();
+        //    Console.WriteLine("Ange efternamn att byta till: ");
+        //    string newlastName = Console.ReadLine();
 
-            updatedContacts = SQL.UpdateContact(findIDTOChange, newfirstName, newlastName);
-        }
+        //    updatedContacts = SQL.UpdateContact(findIDTOChange, newfirstName, newlastName);
+        //}
 
-        private static void AddContact()
-        {
-            Console.WriteLine("Ange förnamn att lägga till: ");
-            string firstName = Console.ReadLine();
+        //private static void AddContact()
+        //{
+        //    Console.WriteLine("Ange förnamn att lägga till: ");
+        //    string firstName = Console.ReadLine();
 
-            Console.WriteLine("Ange efternamn att lägga till: ");
-            string lastName = Console.ReadLine();
+        //    Console.WriteLine("Ange efternamn att lägga till: ");
+        //    string lastName = Console.ReadLine();
 
-            Console.WriteLine(SQL.AddContact(firstName, lastName));
-        }
+        //    Console.WriteLine(SQL.AddContact(firstName, lastName));
+        //}
 
         private static List<Contact> GetAllData()
         {
-            List<Contact> contacts = SQL.GetAllContacts();
+            List<Contact> contacts = SQL.SearchContactIDByName();
 
             foreach (var contact in contacts)
             {
